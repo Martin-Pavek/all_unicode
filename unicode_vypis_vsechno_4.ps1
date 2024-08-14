e0183a825b1b2e9a9de8bed74cf76523e4601265
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2024-08-14 13:15:05 +0200

    Update unicode_vypis_vsechno_4.ps1

--- unicode_vypis_vsechno_4.ps1
+++ unicode_vypis_vsechno_4.ps1
@@ -1,24 +1,11 @@
 ﻿cls
-
 Clear-Variable out_1, uni_1, radek, aa -ErrorAction SilentlyContinue
-
 $out_1 = @()
 
 for ( $aa = 128; $aa -lt 55291; $aa++ ) {
-
-<#
-echo $aa
-$uni_1 = $([char]$aa) # type char
-echo $uni_1
-#>
-
 $radek = "Char $aa : $([char]$aa)"
-#$radek = "$([char]$aa)" # type string 
 echo $radek
-
 $out_1 += $radek
-#"Char $aa : $([char]$aa)"
-
 }
 
 echo $out_1.Length
